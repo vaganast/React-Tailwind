4f10ebd0da3881c4ae078694bee3f2061ee19f5a
vaganast <71605687+vaganast@users.noreply.github.com>
2022-08-16 21:58:18 +0300

    navbar hero components
    add assets , styles, constants and all components ,
    styled navbar and hero

---
+++ src/components/Stats.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { stats } from '../constats'
+import styles from '../style'
+
+const Stats = () => 
+  return (
+    <div>Stats</div>
+  )
+}
+
+export default Stats
